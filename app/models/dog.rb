7adb5edd0ee974e5719aef33c54eeabd4c93bc40
Theresa Cyrus <cyrustheresa@gmail.com>
2019-09-21 21:42:47 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,17 +1,3 @@
 class Dog 
-  attr_accessor :name , :age , :breed
-  
-  @@all=[]
-  
-  def initialize(name, age, breed)
-    @name = name
-    @age = age
-    @breed = breed
-    
-  end
-  
-  def self.all
-    @@all
-  end
   
 end
